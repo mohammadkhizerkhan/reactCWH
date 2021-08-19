13d4dd0facd640fa954cebd05164c7218f48b66a
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-08-20 00:30:58 +0530

    added bootstrap,cleared props

---
+++ module1.mjs
@@ -0,0 +1,7 @@
+const a="khan"
+const b="raj"
+const c="sahish"
+const d="khizer"
+
+export default b;
+export {a,c,d};
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+import ui,{a,c,d} from './module1.mjs';
+
+console.log(ui)
+console.log(a)
+console.log(c)
+console.log(d)
